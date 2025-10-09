434e8fbcd517f7062626a45ff94bf074025b3d66
Marc Oggier <marc.oggier@megavolts.ch>
2025-10-08 17:23:22 -0800

    Minor Update

--- .github/test-21-local_build.sh
+++ .github/test-21-local_build.sh
@@ -5,7 +5,7 @@ GIT_DIR=$(pwd)
 
 # Import buildvars
 BUILD_DATE=$(cat ${GIT_DIR}/buildvars | awk '/^BUILD_DATE=/' | cut -d= -f2)
-BUILD_VERSION=$(cat ${GIT_DIR}/buildvars | awk '/^DOCKER_IMAGE_VERSION=/' | cut -d= -f2)
+BUILD_IMAGE_VERSION=$(cat ${GIT_DIR}/buildvars | awk '/^BUILD_IMAGE_VERSION=/' | cut -d= -f2)
 
 # Check if BUILD_VERSION is already released
 # Check for latest released
@@ -15,21 +15,21 @@ if [[ $(curl -s https://api.github.com/repos/megavolts/lego_cli/releases | grep 
 else
     LATEST_RELEASE=$(curl -s https://api.github.com/repos/megavolts/lego_cli/releases | jq '.[0] | .name' -r | grep -Eo '([0-9]+)(\.?[0-9]+)*' | head -1)
 
-    if [[ $LATEST_RELEASE = $BUILD_VERSION ]];
+    if [[ $LATEST_RELEASE = $BUILD_IMAGE_VERSION ]];
         then
-        echo "Release $BUILD_VERSION already exist. Updated build version with next revisoin"
-        PACKAGE_VERSION=$( $BUILD_VERSION | cut -d- -f1)
-        BUILD_REVISION=$( $BUILD_VERSION | cut -d- -f1)
+        echo "Release $BUILD_IMAGE_VERSION already exist. Updated build version with next revisoin"
+        BUILD_VERSION=$( $BUILD_IMAGE_VERSION | cut -d- -f1)
+        BUILD_REVISION=$( $BUILD_IMAGE_VERSION | cut -d- -f2)
         BUILD_REVISION=$(( $BUILD_REVISION +1 ))
-        BUILD_VERSION=$PACKAGE_VERSION-$BUILD_REVISION
+        BUILD_IMAGE_VERSION=$BUILD_VERSION-$BUILD_REVISION
     fi
 fi
 
-echo "Building docker image version $BUILD_VERSION on $BUILD_DATE"
+echo "Building docker image version $BUILD_IMAGE_VERSION on $BUILD_DATE"
 
 docker buildx build $GIT_DIR/lego_cli/ \
         -t megavolts/lego_cli:testbuild \
-        --build-arg BUILD_VERSION=$BUILD_VERSION \
+        --build-arg BUILD_IMAGE_VERSION=$BUILD_IMAGE_VERSION \
         --build-arg BUILD_DATE=$BUILD_DATE \
         --build-arg TARGETARCH="linux/amd64" \
         --load
